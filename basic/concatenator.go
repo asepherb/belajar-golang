6cbc360fb0e103c145e7433c82c87236465978d6
hendisantika <hendisantika@gmail.com>
2016-11-25 07:25:03 +0700

    concatenator in go

---
+++ basic/concatenator.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+    var fName string = "Hendi"
+    var lName = "Santika"
+    var fullName = fName + " " + lName
+    fmt.Println(fullName)
+    fmt.Println(fName, lName)
+}
